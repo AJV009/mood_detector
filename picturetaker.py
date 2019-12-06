dde0961ead05096d5a74c853779ff5e6773fcca1
Alphons Jaimon <jaimonalphons@gmail.com>
2019-12-06 11:45:45 +0530

    commited

--- picturetaker.py
+++ picturetaker.py
@@ -3,7 +3,7 @@ cam = cv2.VideoCapture(0)
 cv2.namedWindow('Press space to take images!')
 while True:
     ret, frame = cam.read()
-    cv2.imshow('Press space to take a photo',frame)
+    cv2.imshow('Press space to take a photo !',frame)
     key = cv2.waitKey(1)
     if key%256 == 32:
         break

9beb8fd08c4f4c59e15b5bfcedd356d1b86370dd
Alphons Jaimon <jaimonalphons@gmail.com>
2019-12-06 12:03:21 +0530

    commmit now

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+import base64
 app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello World"
+def upload_image():
+    json = request.get_json()
+    base64_image = base64.b64decode(json['image'])
+    return 'OK'

1a16a09deaadcb1fef298a4bb045da64bd23e841
Alphons Jaimon <jaimonalphons@gmail.com>
2019-12-07 13:41:23 +0530

    final chane

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
-import azure.cosmos.cosmos_client as cosmos_client
 import io
-import uuid
 import base64
+import azure.cosmos.cosmos_client as cosmos_client
+import uuid
 
 app = Flask(__name__)
 
 cosmos_url = 'https://mhl-xaj.documents.azure.com:443'
 cosmos_primary_key = '4a6VF6JyWFUplVURKKj9BTeyKZgKz1r4ot2u2QLGvVMOaoJWgeaTAGHTWzWJQfsPYucmsaq1B9DcgRY65ZvDnQ=='
 cosmos_collection_link = 'dbs/workshop/colls/faces'
-client = cosmos_client.CosmosClient(url_connection=cosmos_url, auth = {'masterKey': cosmos_primary_key})
+
+client = cosmos_client.CosmosClient(url_connection=cosmos_url, 
+                                    auth={'masterKey': cosmos_primary_key})
 
 @app.route('/')
 def home():
-  docs = list(client.ReadItems(cosmos_collection_link))
-  return render_template('home.html', result = docs)
+    docs = list(client.ReadItems(cosmos_collection_link))
+    return render_template('home.html', result = docs)
 
 face_api_endpoint = 'https://facefun.cognitiveservices.azure.com/'
-face_api_key = '09d139cc16634316bb4164546f894db9'
+face_api_key = '4fe8f54689c2449e89719d515a9f3247'
 credentials = CognitiveServicesCredentials(face_api_key)
 face_client = FaceClient(face_api_endpoint, credentials=credentials)
 
@@ -30,10 +32,10 @@ def best_emotion(emotion):
     emotions['disgust'] = emotion.disgust
     emotions['fear'] = emotion.fear
     emotions['happiness'] = emotion.happiness
-    emotions['neutral'] = emotion.meutral
+    emotions['neutral'] = emotion.neutral
     emotions['sadness'] = emotion.sadness
     emotions['surprise'] = emotion.surprise
-    return max(zip(emotions.values(),emotions.key()))[1]
+    return max(zip(emotions.values(), emotions.keys()))[1]
 
 def get_emotions():
     docs = list(client.ReadItems(cosmos_collection_link))
@@ -41,18 +43,23 @@ def get_emotions():
     counts = dict()
     for emotion in emotions:
         counts[emotion] = counts.get(emotion, 0) + 1
+
     return jsonify(counts)
 
-@app.route('/image',methods=['POST'])
+@app.route('/image', methods=['POST'])
 def upload_image():
     json = request.get_json()
     base64_image = base64.b64decode(json['image'])
+
     image = io.BytesIO(base64_image)
-    faces = face_client.face.detect_with_stream(image,return_face_attributes=['emotion'])
+    faces = face_client.face.detect_with_stream(image,
+                                                return_face_attributes=['emotion'])
+
     for face in faces:
         doc = {
-            'id':str(uuid.uuid4()),
-            'emotion':best_emotion(face.face_attributes.emotion)
-        }
+                'id' : str(uuid.uuid4()),
+                'emotion': best_emotion(face.face_attributes.emotion)
+              }
         client.CreateItem(cosmos_collection_link, doc)
+
     return get_emotions()
--- picturetaker.py
+++ picturetaker.py
@@ -3,26 +3,27 @@ import requests
 import base64
 import json
 
+imageUrl = 'https://xaj-pyweb-app.azurewebsites.net/image'
+
 def check_sadness(emotions):
     if 'sadness' in emotions:
         if emotions['sadness'] >= 3:
-            print("Are you feeling okay? Do you want to talk about it?")
-
-imageUrl = 'https://xaj-pyweb-app.azurewebsites.net/image'
+            print('Are you feeling ok? Do you want to talk about it?')
 
 def upload(frame):
     data = {}
-    img = cv2.imencode('.jpg',frame)[1]
+    img = cv2.imencode('.jpg', frame)[1]
     data['image'] = base64.b64encode(img).decode()
     results = requests.post(url=imageUrl, json=data)
-    print(results.text())
+    check_sadness(results.json())
 
 cam = cv2.VideoCapture(0)
-cv2.namedWindow('Press space to take images!')
+cv2.namedWindow('Press space to take a photo')
 
 while True:
     ret, frame = cam.read()
-    cv2.imshow('Press space to take a photo !',frame)
+    cv2.imshow('Press space to take a photo', frame)
+        
     key = cv2.waitKey(1)
     if key%256 == 32:
         upload(frame)
